c93c5e650ff1a78161105150a9afbb1f61551ce8
prajit7-fyers <prajit.bhat@fyers.in>
2026-05-14 12:53:44 +0530

    PR fix

--- helpers.go
+++ helpers.go
@@ -1,15 +1,15 @@
 package talib
 
-type PriceSource int
+type PriceSource string
 
 const (
-	Open PriceSource = iota
-	High
-	Low
-	Close
-	HL2
-	HLC3
-	OHLC4
+	Open  PriceSource = "Open"
+	High  PriceSource = "High"
+	Low   PriceSource = "Low"
+	Close PriceSource = "Close"
+	HL2   PriceSource = "HL2"
+	HLC3  PriceSource = "HLC3"
+	OHLC4 PriceSource = "OHLC4"
 )
 
 func GetPrice(open, high, low, close []float64, i int, src PriceSource) float64 {
